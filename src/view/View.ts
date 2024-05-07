77afe540df48fa4c51f3ae154d31b1cf8d883900
SunFlow <sunflow.riven@gmail.com>
2024-05-07 21:48:58 +0200

    centralized local storage key

--- src/view/View.ts
+++ src/view/View.ts
@@ -1,13 +1,16 @@
-import Pity from '../logic/Pity.js';
 import Controller from '../controller/Controller.js';
 import EnchantmentItem, { ENCHANTMENT_ITEMS } from '../logic/EnchantmentItem.js';
 import EnchantmentMaterial, { ENCHANTMENT_MATERIALS, EnchantmentMaterialShadowed } from '../logic/EnchantmentMaterial.js';
+import { ENCHANTMENT_PRESETS } from '../logic/EnchantmentPreset.js';
 import { FailStack } from '../logic/FailStack.js';
+import Pity from '../logic/Pity.js';
 import Logger from '../util/Logger.js';
 import { nf_commas, nonNullElement, nonNullElementAll } from '../util/util.js';
-import EnchantmentPreset, { ENCHANTMENT_PRESETS } from '../logic/EnchantmentPreset.js';
 
 export default class View {
+	private readonly DEVELOPING = true;
+	private readonly LOCAL_STORAGE_KEY = `bdo-enchantment-simulator${this.DEVELOPING ? '-dev' : ''}`;
+
 	private controller!: Controller;
 
 	private cbScaleOutput;
@@ -573,16 +576,16 @@ export default class View {
 
 	saveState(state: SimulatorState) {
 		const profile = this.sProfile.value || 'default';
-		const oldJson = localStorage.getItem('bdo-enchantment-simulator-dev');
+		const oldJson = localStorage.getItem(this.LOCAL_STORAGE_KEY);
 		const newAppState = new AppState(profile, state, oldJson);
 		const newJson = JSON.stringify(newAppState);
-		localStorage.setItem('bdo-enchantment-simulator-dev', newJson);
+		localStorage.setItem(this.LOCAL_STORAGE_KEY, newJson);
 		// console.log('saved', newAppState, newJson);
 	}
 
 	loadState() {
 		const profile = this.sProfile.value || 'default';
-		const appJson = localStorage.getItem('bdo-enchantment-simulator-dev');
+		const appJson = localStorage.getItem(this.LOCAL_STORAGE_KEY);
 		if (!appJson) return Logger.error('No App-State found');
 		const appState: AppState = JSON.parse(appJson);
 		// console.log('loaded', appState);
